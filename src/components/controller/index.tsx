9f8d83488c2f866c4e70b949056a0b2310f90f4d
Kayode Oluwasegun <segunkayode00@gmail.com>
2022-12-16 21:15:43 +0100

    add currently playing song

--- src/components/controller/index.tsx
+++ src/components/controller/index.tsx
@@ -1,29 +1,53 @@
 import React, { useState } from "react";
 import { Button } from "../button";
-import { MediaControllerContainer } from "./styled";
-import { FaPlay, FaPause, FaForward, FaBackward } from "react-icons/fa";
+import { MediaControllerContainer, CurrentlyPlayingContainer } from "./styled";
+import {
+  FaPlay,
+  FaPause,
+  FaForward,
+  FaBackward,
+  FaMusic,
+} from "react-icons/fa";
+
+interface CurrentlyPlayingProps {
+  songName?: string;
+}
+
+export const CurrentlyPlaying: React.FC<CurrentlyPlayingProps> = ({
+  songName,
+}) => {
+  return (
+    <CurrentlyPlayingContainer>
+      <FaMusic />
+      <p>{songName || ""}</p>
+    </CurrentlyPlayingContainer>
+  );
+};
 
 export const MediaController: React.FC = () => {
   const [isPlaying, setIsPlaying] = useState(false);
 
   return (
-    <MediaControllerContainer>
-      <Button size="large" variant="outlined">
-        <FaBackward />
-      </Button>
-      <Button
-        variant="outlined"
-        size="large"
-        onClick={() => {
-          console.log("working");
-          setIsPlaying((x) => !x);
-        }}
-      >
-        {isPlaying ? <FaPause /> : <FaPlay />}
-      </Button>
-      <Button variant="outlined" size="large">
-        <FaForward />
-      </Button>
-    </MediaControllerContainer>
+    <>
+      <CurrentlyPlaying songName="Jesus is Lord - Drake" />
+      <MediaControllerContainer>
+        <Button size="large" variant="outlined">
+          <FaBackward />
+        </Button>
+        <Button
+          variant="outlined"
+          size="large"
+          onClick={() => {
+            console.log("working");
+            setIsPlaying((x) => !x);
+          }}
+        >
+          {isPlaying ? <FaPause /> : <FaPlay />}
+        </Button>
+        <Button variant="outlined" size="large">
+          <FaForward />
+        </Button>
+      </MediaControllerContainer>
+    </>
   );
 };
--- src/components/controller/styled.ts
+++ src/components/controller/styled.ts
@@ -19,3 +19,23 @@ export const MediaControllerContainer = styled.div`
     font-size: 60px;
   }
 `;
+
+export const CurrentlyPlayingContainer = styled.div`
+  color: #fff;
+  position: absolute;
+  bottom: 20px;
+  height: 100px;
+  left: 40px;
+  font-size: 24px;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+
+  & p {
+    transform: translateY(-25%);
+    margin: 0;
+    border-bottom: 1px solid #fff;
+  }
+`;
